52f75ec7a7255f543e38d0a307e89ad43e5f3aab
Sai2963 <venkatsai0629@gmail.com>
2024-12-24 20:56:50 +0530

    Storing Variables

---
+++ site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $CharName = "DDK";
+        $CharAge = 20;
+        echo "<h1>name : $CharName</h1>";
+        echo "<h1>age : $CharAge</h1>";
+        $CharName = "D V Sai ";
+        $CharAge = 21;
+        echo "<h1>name : $CharName</h1>";
+        echo "<h1>age : $CharAge</h1>";
+    ?>
+</body>
+</html>
