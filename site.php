4c7eec51b53e23200f99c0cba8af25afe55f0df9
Sai2963 <venkatsai0629@gmail.com>
2024-12-26 06:58:09 +0530

    object functions & Getters and Setters

--- site.php
+++ site.php
@@ -291,9 +291,34 @@ echo "<br>";
     echo "<br>";
     echo $book2->title;                                                                     
     echo "<br>";
-    
+
 
     ?>
+      <!-- Object Functions -->
+  <?php
+    class Student {
+      var $name;
+      var $major;
+      var $gpa;
+
+      function __construct($name, $major, $gpa) {
+        $this->name = $name;
+        $this->major = $major;
+        $this->gpa = $gpa;
+      }
+
+      function hasHonors() {
+        if ($this->gpa  >= 3.5) {
+          return "true";
+        }
+        return "false";
+      }
+    }
+    $student1 = new Student("Jim", "Business", 2.8);
+    $student2 = new Student("Pam", "Art", 3.6);
+    // $student2->gpa = 2.5;
+    echo $student2->hasHonors();
+  ?>
 
 </body>
 </html>
